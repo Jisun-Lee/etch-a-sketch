9a1063684e652002f3052c27172113933883030d
Jisun <117595989+Jisun-Lee@users.noreply.github.com>
2023-07-18 13:14:12 -0700

    Fixes gridSetup and draw functions

--- script.js
+++ script.js
@@ -3,82 +3,44 @@ const DEFAULT_SIZE = 16;
 const container = document.querySelector('.container');
 const input = document.querySelector('input');
 const output = document.querySelector('output');
-output.innerHTML = `${input.value} x ${input.value}`; //shows default slider value
+//const toggle = document.getElementById('eraser-toggle');
+
+//shows default slider value
+output.innerHTML = `${input.value} x ${input.value}`; 
 
 //default grid on load
-window.addEventListener('load', gridDraw(DEFAULT_SIZE));
+window.addEventListener('load', gridSetup(DEFAULT_SIZE));
 
-//draws new grid every time slider is moved
+//clears and draws new grid every time slider is moved
 input.addEventListener('input', function () {
   output.innerHTML = `${input.value} x ${input.value}`;
   clearScreen();
-  gridDraw(input.value);
+  gridSetup(input.value);
 });
 
-function gridDraw(x) {
+function gridSetup(x) {
+    for (let i = 0; i < x * x; i++) {
+    const box = document.createElement('div');
+    box.classList.add('box');
+    container.appendChild(box);
+    }
   container.style.gridTemplateColumns = `repeat(${x}, 1fr)`;
   container.style.gridTemplateRows = `repeat(${x}, 1fr)`;
-  for (i = 0; i < x * x; i++) {
-    const box = document.createElement('div');
-    container.appendChild(box);
-    //box.className = `box${i}`;
-    //box.innerText = `${i}`; //temp #s div boxes so I can see changes
-    box.addEventListener('mouseover', function() {
-      box.classList.add('shift');
-    })
+  draw();
   }
-}
 
 function clearScreen() {
   while (container.firstChild) {
     container.removeChild(container.lastChild);
   }
-  gridDraw(input.value);
+  gridSetup(input.value);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*const boxes = [];
-
-// creates the div boxes.
-for (let i = 1; i <= 256; i++) {
-  const divBox = {
-    id: i,
-    name: `divBox${i}`,
-  };
-  boxes.push(divBox.name);
+function draw() {
+  const gridElements = document.querySelectorAll('.box');
+  gridElements.forEach((e) => {
+    e.addEventListener ('mouseover', () => {
+      e.classList.add('black');
+    })
+  })
 }
-
-console.log(boxes);
-*/

5e1368d11a24410befa7e8682f1580503bafed99
Jisun <117595989+Jisun-Lee@users.noreply.github.com>
2023-07-19 17:47:01 -0700

    Makes color picking buttons functional

--- script.js
+++ script.js
@@ -1,45 +1,76 @@
 const DEFAULT_SIZE = 16;
+const DEFAULT_COLOR = 'black';
+
+let currentColor = DEFAULT_COLOR;
 
 const container = document.querySelector('.container');
 const input = document.querySelector('input');
 const output = document.querySelector('output');
-//const toggle = document.getElementById('eraser-toggle');
+const gridElements = document.querySelectorAll('.box');
+const black = document.querySelector('.black');
+const rainbow = document.querySelector('.rainbow');
+const gray = document.querySelector('.gray');
+const erase = document.querySelector('.erase');
 
-//shows default slider value
-output.innerHTML = `${input.value} x ${input.value}`; 
-
-//default grid on load
-window.addEventListener('load', gridSetup(DEFAULT_SIZE));
+//default load screen
+window.addEventListener('load', () => {
+  //shows default slider value
+  output.innerHTML = `${input.value} x ${input.value}`; 
+  colorSelection();
+  gridSetup(DEFAULT_SIZE);
+  draw(DEFAULT_COLOR);
+});
 
 //clears and draws new grid every time slider is moved
 input.addEventListener('input', function () {
   output.innerHTML = `${input.value} x ${input.value}`;
   clearScreen();
+  colorSelection();
+  draw(currentColor);
 });
 
 function gridSetup(x) {
-    for (let i = 0; i < x * x; i++) {
+  for (let i = 0; i < x * x; i++) {
     const box = document.createElement('div');
     box.classList.add('box');
     container.appendChild(box);
-    }
+  }
   container.style.gridTemplateColumns = `repeat(${x}, 1fr)`;
   container.style.gridTemplateRows = `repeat(${x}, 1fr)`;
-  draw();
-  }
+}
+
+function draw(color) {
+  color = currentColor;
+  const gridElements = document.querySelectorAll('.box');
+  gridElements.forEach((e) => {
+    e.addEventListener('mouseover', () => {
+      e.style.backgroundColor = `${color}`;
+    })
+  })
+}
+
+function colorSelection() {
+  const buttons = document.querySelectorAll('.button');
+  buttons.forEach((e) => {
+    e.addEventListener('click', () => {
+      if (e.value == 'grayscale') {
+        return;
+      } else {
+      currentColor = `${e.value}`;
+      draw(currentColor);
+      }
+    })
+  })
+}
 
 function clearScreen() {
   while (container.firstChild) {
     container.removeChild(container.lastChild);
   }
   gridSetup(input.value);
-}
-
-function draw() {
-  const gridElements = document.querySelectorAll('.box');
-  gridElements.forEach((e) => {
-    e.addEventListener ('mouseover', () => {
-      e.classList.add('black');
-    })
-  })
+  console.log(currentColor);
+  if (currentColor === 'white') {
+    currentColor = DEFAULT_COLOR;
+  } 
+  draw(currentColor);
 }
